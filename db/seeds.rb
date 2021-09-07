3b4860117938eea9644cf608769717c4e9d55e3e
jasonronalddavis <jdavis@c.ringling.edu>
2021-09-07 02:08:49 -0500

    Item resource seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+toy = Item.create(type: "Ball")
+toy = Item.create(type: "Bone")
+toy = Item.create(type: "Science")
